ed1f0ea3aa421a418e30e3e4c4b10aac5dc399b1
Erin Sparling <erin@everyplace.net>
2022-04-25 18:43:12 +0100

    Now using python-shell for inter-process communication

--- class.Screen.js
+++ class.Screen.js
@@ -1,9 +1,19 @@
+import { PythonShell }  from 'python-shell'
+
+let display = new PythonShell('display.py', {
+  pythonOptions: ['-u']
+})
+
 class Screen {
   constructor(location = 'images/'){
     this.counter = 0
     this.limit = 10
     this.location = location
-    this.render = this.display
+    this.render = this.shell
+  }
+
+  shell (p = this.p, canvas=this.canvas) {
+    display.send(canvas.canvas.toDataURL())
   }
 
   display (p = this.p, canvas=this.canvas) {
